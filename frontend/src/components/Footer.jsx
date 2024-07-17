b3677ffe12ac49ef2a00dff1ddfe150bbf468803
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-07-17 10:15:57 -0300

    feat: add Footer component to project

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+      
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODEWITHZEESHU</p>
+          </div>
+          <div className="right">
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
